50a0b69f131f5f869b80374a5e0e50ab0321cb80
hendusoone <hendricksond@gmail.com>
2018-12-27 19:30:21 -0500

    Strip special characters out of filename for text endpoint

--- app/controllers/api/v2/text_controller.rb
+++ app/controllers/api/v2/text_controller.rb
@@ -2,9 +2,9 @@ class API::V2::TextController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def index
-        render status: :not_found, json: {error: 'file not found'}  and return unless File.exists?("public/text/#{params['filename']}.json")
-        
-        file = File.read("public/text/#{params['filename']}.json")
+        filename = params['filename'].strip.downcase.gsub(/[^\w-]/, '')
+        render status: :not_found, json: {error: 'file not found'}  and return unless File.exists?("public/text/#{filename}.json")
+        file = File.read("public/text/#{filename}.json")
         render json: file
     end
 end

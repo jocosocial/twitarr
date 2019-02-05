a675071e08fab0794f6c9bf5bd7a3ad59a210958
hendusoone <hendricksond@gmail.com>
2019-02-05 03:20:43 -0500

    Added announcement endpoints: get by id, update, delete

--- app/controllers/api/v2/admin_controller.rb
+++ app/controllers/api/v2/admin_controller.rb
@@ -3,6 +3,7 @@ class API::V2::AdminController < ApplicationController
 	
 	before_filter :admin_required
 	before_filter :fetch_user, :only => [:profile, :update_user, :activate, :reset_password, :reset_photo]
+	before_filter :fetch_announcement, :only => [:announcement, :update_announcement, :delete_announcement]
 	
 	def users
 		render json: {status: 'ok', list: User.all.asc(:username).map { |x| x.decorate.admin_hash }}
@@ -59,7 +60,7 @@ class API::V2::AdminController < ApplicationController
 	end
 	
 	def announcements
-		render json: {status: 'ok', list: Announcement.all.desc(:timestamp).map { |x| x.decorate.to_admin_hash }}
+		render json: {status: 'ok', list: Announcement.all.desc(:timestamp).map { |x| x.decorate.to_admin_hash(request_options) }}
 	end
 	
 	def new_announcement
@@ -78,8 +79,46 @@ class API::V2::AdminController < ApplicationController
 
 		render status: :bad_request, json: {status: 'error', errors: errors} and return unless errors.length == 0
 		
-		Announcement.create(author: current_username, text: params[:text], timestamp: time, valid_until: valid_until)
-		render json: {status: 'ok'}
+		announcement = Announcement.create(author: current_username, text: params[:text], timestamp: time, valid_until: valid_until)
+		render json: {status: 'ok', announcement: announcement.decorate.to_admin_hash(request_options)}
+	end
+
+	def announcement
+		render json: {status: 'ok', announcement: @announcement.decorate.to_admin_hash(request_options)}
+	end
+
+	def update_announcement
+		time = Time.now
+		errors = []
+
+		errors.push('Text is required.') if params[:text].nil? || params[:text].empty?
+
+		begin
+			valid_until = Time.from_param(params[:valid_until])
+		rescue
+			errors.push('Unable to parse valid until.')
+		else
+			errors.push('Valid until must be in the future.') unless valid_until > time
+		end		
+
+		render status: :bad_request, json: {status: 'error', errors: errors} and return unless errors.length == 0
+		
+		@announcement.text = params[:text]
+		@announcement.valid_until = valid_until
+		
+		if @announcement.save
+			render json: {status: 'ok', announcement: @announcement.decorate.to_admin_hash(request_options)}
+		else
+			render status: :bad_request json: {status: 'error', errors: @announcement.errors.full_messages}
+		end
+	end
+
+	def delete_announcement
+		if @announcement.destroy
+      render json: {status: 'ok'}
+    else
+      render status: :bad_request, json: {status: 'error', error: @announcement.errors.full_messages}
+    end
 	end
 	
 	def upload_schedule
@@ -97,5 +136,13 @@ class API::V2::AdminController < ApplicationController
 	def fetch_user
     @user = User.get params[:username]
     render status: :not_found, json: {status: 'error', error: 'User not found.'} and return unless @user
+	end
+	
+	def fetch_announcement
+		begin
+			@announcement = Announcement.find(params[:id])
+		rescue
+			render status: :not_found, json: {status: 'error', error: 'Announcement not found.'} and return
+		end
   end
 end

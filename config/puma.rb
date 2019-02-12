b9046bcd27492fef3862a120bef3c242bf9cdf26
hendusoone <hendricksond@gmail.com>
2019-02-12 14:54:18 -0500

    Moved puma configurations, updated thread configs

--- config/puma.rb
+++
@@ -1,5 +0,0 @@
-environment 'production'
-
-quiet
-threads 0, 16
-bind 'tcp://0.0.0.0:4000'
---
+++ config/puma/production.rb
@@ -0,0 +1,7 @@
+environment 'production'
+
+directory '/var/www/twitarr'
+pidfile 'tmp/puma.pid'
+bind 'unix:///var/www/twitarr/tmp/puma.sock'
+quiet
+threads 8, 64

84fedcd60bc375f9d39e4ec2735e6d7eae1b0f45
hendusoone <hendricksond@gmail.com>
2019-10-09 21:04:27 -0400

    Fixed some rubocop offenses

--- Rakefile
+++ Rakefile
@@ -1,10 +1,10 @@
 # Add your own tasks in files placed in lib/tasks ending in .rake,
 # for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
 
-require File.expand_path('../config/application', __FILE__)
+require File.expand_path('config/application', __dir__)
 
 Rails.application.load_tasks
 
-system_task = Rake::Task["test:system"]
+system_task = Rake::Task['test:system']
 test_task = Rake::Task[:test]
 test_task.enhance { system_task.invoke }
--- perf_test.rb
+++ perf_test.rb
@@ -4,42 +4,42 @@ require 'net/http'
 require 'json'
 require 'peach'
 
-SERVER_URL = 'http://localhost:3000'
+SERVER_URL = 'http://localhost:3000'.freeze
 # SERVER_URL = 'https://twitarr.rylath.net'
 
 requests = [
-    Proc.new do |http|
-      response = http.request Net::HTTP::Get.new(SERVER_URL + '/posts/all')
-      Rails.logger.info "ALL: #{response.msg}"
-      data = JSON.parse(response.body)
-      photos = data['list'].map { |x| x['data']['photos'] }.flatten.compact!
-      photos.each do |photo|
-        photo_response = http.request(Net::HTTP::Get.new("#{SERVER_URL}/img/photos/sm_#{photo}"))
-        Rails.logger.info "PHOTO #{photo}: #{photo_response.msg}"
-      end
-      1 + photos.size
-    end,
-    Proc.new do |http|
-      response = http.request Net::HTTP::Get.new(SERVER_URL + '/posts/popular')
-      Rails.logger.info "POPULAR: #{response.msg}"
-      data = JSON.parse(response.body)
-      photos = data['list'].map { |x| x['data']['photos'] }.flatten.compact!
-      photos.each do |photo|
-        photo_response = http.request(Net::HTTP::Get.new("https://twitarr.rylath.net/img/photos/sm_#{photo}"))
-        Rails.logger.info "PHOTO #{photo}: #{photo_response.msg}"
-      end
-      1 + photos.size
-    end,
-    Proc.new do |http|
-      response = http.request Net::HTTP::Get.new(SERVER_URL + '/user/ac?string=g')
-      Rails.logger.info "AUTOCOMPLETE: #{response.msg}"
-      1
-    end,
-    Proc.new do |http|
-      response = http.request Net::HTTP::Get.new(SERVER_URL + '/api/v1/user/auth?username=kvort&password=foobar')
-      Rails.logger.info "AUTH: #{response.msg}"
-      1
+  Proc.new do |http|
+    response = http.request Net::HTTP::Get.new(SERVER_URL + '/posts/all')
+    Rails.logger.info "ALL: #{response.msg}"
+    data = JSON.parse(response.body)
+    photos = data['list'].map { |x| x['data']['photos'] }.flatten.compact!
+    photos.each do |photo|
+      photo_response = http.request(Net::HTTP::Get.new("#{SERVER_URL}/img/photos/sm_#{photo}"))
+      Rails.logger.info "PHOTO #{photo}: #{photo_response.msg}"
     end
+    1 + photos.size
+  end,
+  Proc.new do |http|
+    response = http.request Net::HTTP::Get.new(SERVER_URL + '/posts/popular')
+    Rails.logger.info "POPULAR: #{response.msg}"
+    data = JSON.parse(response.body)
+    photos = data['list'].map { |x| x['data']['photos'] }.flatten.compact!
+    photos.each do |photo|
+      photo_response = http.request(Net::HTTP::Get.new("https://twitarr.rylath.net/img/photos/sm_#{photo}"))
+      Rails.logger.info "PHOTO #{photo}: #{photo_response.msg}"
+    end
+    1 + photos.size
+  end,
+  Proc.new do |http|
+    response = http.request Net::HTTP::Get.new(SERVER_URL + '/user/ac?string=g')
+    Rails.logger.info "AUTOCOMPLETE: #{response.msg}"
+    1
+  end,
+  Proc.new do |http|
+    response = http.request Net::HTTP::Get.new(SERVER_URL + '/api/v1/user/auth?username=kvort&password=foobar')
+    Rails.logger.info "AUTH: #{response.msg}"
+    1
+  end
 ]
 
 class Array
@@ -48,7 +48,7 @@ class Array
   end
 end
 
-if __FILE__ == $0
+if $PROGRAM_NAME == __FILE__
 
   signal = false
   lock = Mutex.new
@@ -60,8 +60,8 @@ if __FILE__ == $0
   threads = 10.times.map do
     Thread.new do
       count = 0
-      while (!signal) do
-        Net::HTTP.start('twitarr.rylath.net', 443, :use_ssl => true, :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |http|
+      until signal
+        Net::HTTP.start('twitarr.rylath.net', 443, use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_NONE) do |http|
           count += requests[Random.rand requests.size].call http
         end
       end
@@ -73,12 +73,10 @@ if __FILE__ == $0
     Rails.logger.info 'Shutting down'
     signal = true
     threads.each do |thread|
-      begin
-        thread.join
-        Rails.logger.info 'Ended correctly'
-      rescue => err
-        Rails.logger.error err.inspect
-      end
+      thread.join
+      Rails.logger.info 'Ended correctly'
+    rescue StandardError => e
+      Rails.logger.error e.inspect
     end
     end_time = Time.now
     Rails.logger.info "TOTAL COUNT: #{total_count} in time: #{end_time - start_time} seconds"

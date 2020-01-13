ff7b9dc89fcc9a8d8ddbfd3be4d95b00576d7c7e
hendusoone <hendricksond@gmail.com>
2020-01-12 21:54:30 -0500

    Remove some unnecessary comments and newlines

--- app/models/photo_metadata.rb
+++ app/models/photo_metadata.rb
@@ -24,21 +24,6 @@
 #
 
 class PhotoMetadata < ApplicationRecord
-  # field :up, as: :uploader, type: String
-  # field :ct, as: :content_type, type: String
-  # field :fn, as: :store_filename, type: String
-  # field :ut, as: :upload_time, type: Time
-  # field :an, as: :animated, type: Boolean
-  # field :hsh, as: :md5_hash, type: String
-  # field :si, as: :sizes, type: Hash, default: {}
-
-  # def serializable_hash(options)
-  #   original_hash = super(options)
-  #   Hash[original_hash.map {|k, v|
-  #          [self.aliased_fields.invert[k] || k , as_str(v)]
-  #        }]
-  # end
-
   belongs_to :user
   has_many :post_photos, dependent: :destroy
 end

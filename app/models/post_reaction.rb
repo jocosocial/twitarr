607b1ee476363434d73100009cbd611c75d7f45e
hendusoone <hendricksond@gmail.com>
2022-02-11 17:49:55 -0500

    Remove redundant presence validator

--- app/models/post_reaction.rb
+++ app/models/post_reaction.rb
@@ -29,7 +29,7 @@ class PostReaction < ApplicationRecord
   belongs_to :stream_post, inverse_of: :post_reactions, optional: true
   belongs_to :user, inverse_of: :post_reactions
 
-  validates :reaction_id, :user_id, presence: true
+  validates :reaction_id, :user_id
   validate :validate_association
 
   default_scope { includes(:user, :reaction).references(:users, :reactions) }
